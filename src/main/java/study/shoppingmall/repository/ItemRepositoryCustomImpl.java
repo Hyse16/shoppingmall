9c24049044741a3e1d64526cabc86ed7e51a8ea5
Hyse16 <gustmd4296@hanmail.net>
2022-11-04 13:42:44 +0900

    ItemRepositoryCustomImpl 추가

---
+++ src/main/java/study/shoppingmall/repository/ItemRepositoryCustomImpl.java
@@ -0,0 +1,68 @@
+package study.shoppingmall.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
+import study.shoppingmall.domain.Item;
+import study.shoppingmall.domain.ItemStatus;
+import study.shoppingmall.domain.QItem;
+import study.shoppingmall.dto.ItemSearchDto;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ItemRepositoryCustomImpl implements  ItemRepositoryCustom{
+    private JPAQueryFactory queryFactory;
+
+    public ItemRepositoryCustomImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    private BooleanExpression searchSellStatusEq(ItemStatus searchSellStatus) {
+        return searchSellStatus == null ? null : QItem.item.itemStatus.eq(searchSellStatus);
+    }
+
+    private BooleanExpression regDtsAfter(String searchDateType) {
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        switch (searchDateType) {
+            case "1d" : dateTime = dateTime.minusDays(1); break;
+            case "1w" : dateTime = dateTime.minusWeeks(1); break;
+            case "1m" : dateTime = dateTime.minusMonths(1); break;
+            case "6m" : dateTime = dateTime.minusMonths(6); break;
+            default: return null; //all, null
+        }
+
+        return QItem.item.regTime.after(dateTime);
+    }
+
+
+    private BooleanExpression searchByLike(String searchBy, String searchQuery){
+        if(StringUtils.equals("itemNm", searchBy)){
+            return QItem.item.name.like("%" + searchQuery + "%");
+        } else if(StringUtils.equals("createdBy", searchBy)){
+            return QItem.item.createdBy.like("%" + searchQuery + "%");
+        }
+        return null;
+    }
+
+    @Override
+    public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+        List<Item> content = queryFactory
+                .selectFrom(QItem.item)
+                .where(regDtsAfter(itemSearchDto.getSearchDateType()),
+                        searchSellStatusEq(itemSearchDto.getSearchSellStatus()),
+                        searchByLike(itemSearchDto.getSearchBy(), itemSearchDto.getSearchQuery()))
+                .orderBy(QItem.item.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        long total = content.size();
+        return new PageImpl<>(content, pageable, total);
+    }
+
+}
